3b25116c2d16cb097e413b6b3a59bec95e66391b
Tomas Kislan <kislan.tomas@gmail.com>
2014-09-29 17:40:04 +0200

    Parsing torrent name from magnet uri

--- api/services/Torrents.js
+++ api/services/Torrents.js
@@ -1,5 +1,13 @@
+var magneturi = require('magnet-uri');
+
 exports.get = function(cb) {
   sails.db.all('SELECT ROWID, magnet, time_added FROM torrents ORDER BY time_added DESC', function(err, torrents) {
-    cb(err, torrents);
+    if (err) return cb(err);
+
+    for (var i = 0; i < torrents.length; i++) {
+      torrents[i]['name'] = magneturi(torrents[i].magnet).name;
+    }
+
+    cb(null, torrents);
   });
 }
