dcd7b98969547948e3b911d209e5821c46257d98
Tomas Kislan <kislan.tomas@gmail.com>
2014-09-29 19:23:15 +0200

    Using foreach instead of for loop

--- api/services/Torrents.js
+++ api/services/Torrents.js
@@ -4,9 +4,9 @@ exports.get = function(cb) {
   sails.db.all('SELECT ROWID, magnet, time_added FROM torrents ORDER BY time_added DESC', function(err, torrents) {
     if (err) return cb(err);
 
-    for (var i = 0; i < torrents.length; i++) {
-      torrents[i]['name'] = magneturi(torrents[i].magnet).name;
-    }
+    torrents.forEach(function(torrent) {
+      torrent['name'] = magneturi(torrent.magnet).name;
+    });
 
     cb(null, torrents);
   });
